65d44f7e2ab2416bbe6c65dcfce7a5d9ae86563d
Oleg Grenrus <oleg.grenrus@iki.fi>
2026-01-04 21:34:42 +0200

    Support GHC-9.14

--- test-suite/Orphans.hs
+++ test-suite/Orphans.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Orphans () where
 
 import Numeric.Natural (Natural)
 import Test.QuickCheck (Arbitrary (..))
 
+#if !MIN_VERSION_QuickCheck(2,17,0)
 -- | The QuickCheck-2.10 doesn't define the Arbitrary Natural instance
 -- We define own instance (and not use quickcheck-instance) to break
 -- the cycle in tests.
 instance Arbitrary Natural where
     arbitrary = fmap (fromInteger . abs) arbitrary
     shrink = map (fromInteger . abs) . shrink . fromIntegral
+#endif
